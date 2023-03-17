6a84f407bc821c4e963517d54c5e442558980aef
ravi991 <raviranjancs4@gmail.com>
2023-03-17 13:02:30 -0400

    Update MutipleLogin.java

--- MavenProject/src/test/java/stepDefinitions/MutipleLogin.java
+++ MavenProject/src/test/java/stepDefinitions/MutipleLogin.java
@@ -12,13 +12,13 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class MutipleLogin {
-	WebDriver driver;
+	WebDriver browserObject;
 	
 	@Given("^I am on the demo site login \"([^\"]*)\"$")
 	public void i_am_on_the_demo_site_login(String string) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-		driver = new ChromeDriver();
+		browserObject = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(string);
 	}
@@ -26,8 +26,8 @@ public class MutipleLogin {
 	@When("^I enter userName = \"([^\"]*)\" and password = \"([^\"]*)\"$")
 	public void i_enter_userName_and_password(String string, String string2) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		WebElement username = driver.findElement(By.name("Email"));
-		WebElement password = driver.findElement(By.name("Password"));
+		WebElement username = browserObject.findElement(By.name("Email"));
+		WebElement password = browserObject.findElement(By.name("Password"));
 		username.clear();
 		username.sendKeys(string);
 		password.clear();
@@ -37,14 +37,14 @@ public class MutipleLogin {
 	@When("^I click login button$")
 	public void i_click_login_button() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		WebElement submitBtn = driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button"));
+		WebElement submitBtn = browserObject.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button"));
 		submitBtn.click();
 	}
 
 	@Then("^text to display \"([^\"]*)\" text$")
 	public void text_to_display_text(String string) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		String actualValue=driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[1]/h1")).getText();
+		String actualValue=browserObject.findElement(By.xpath("/html/body/div[3]/div[1]/div[1]/h1")).getText();
 		assertEquals(actualValue,string);
 	}
 
