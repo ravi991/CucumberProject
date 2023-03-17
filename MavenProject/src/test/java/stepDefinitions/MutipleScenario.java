b13f747f817019f4272bb4442af269106098e44f
ravi991 <raviranjancs4@gmail.com>
2023-03-17 13:04:19 -0400

    Update MutipleScenario.java

--- MavenProject/src/test/java/stepDefinitions/MutipleScenario.java
+++ MavenProject/src/test/java/stepDefinitions/MutipleScenario.java
@@ -8,27 +8,27 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class MutipleScenario {
-WebDriver driver;
+WebDriver browserObject;
 	@Given("^user is on Google home page \"([^\"]*)\"$")
 	public void user_is_on_Google_home_page(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get(arg1);
+        browserObject = new ChromeDriver();
+        browserObject.get(arg1);
         System.out.println(arg1 + "\t is the site");
 	}
 
 	@Then("^user enters keyword \"([^\"]*)\"$")
 	public void user_enters_keyword(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		driver.findElement(By.name("q")).sendKeys(arg1);
+		browserObject.findElement(By.name("q")).sendKeys(arg1);
         System.out.println(arg1 + "\t is the keyword");
 	}
 
 	@Then("^user submits the page$")
 	public void user_submits_the_page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		driver.findElement(By.name("btnK")).submit();
+		browserObject.findElement(By.name("btnK")).submit();
         System.out.println("\t submit here");
 	}
 
@@ -37,15 +37,15 @@ WebDriver driver;
 	    // Write code here that turns the phrase above into concrete actions
 		Thread.sleep(arg1 * 1000);
         System.out.println("wait of 5 secs");
-        driver.close();
+        browserObject.close();
 	}
 
 	@Given("^user is on GitHub home page \"([^\"]*)\"$")
 	public void user_is_on_GitHub_home_page(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Program Files\\\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get(arg1);
+        browserObject = new ChromeDriver();
+        browserObject.get(arg1);
         System.out.println(arg1 + "\t is the site");
 	}
 
@@ -54,15 +54,15 @@ WebDriver driver;
 	    // Write code here that turns the phrase above into concrete actions
 		Thread.sleep(arg1 * 1000);
         System.out.println("wait of 5 secs");
-        driver.close();
+        browserObject.close();
 	}
 
 	@Given("^user is on Twitter home page \"([^\"]*)\"$")
 	public void user_is_on_Twitter_home_page(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		 System.setProperty("webdriver.chrome.driver", "C:\\\\Program Files\\\\chromedriver.exe");
-	        driver = new ChromeDriver();
-	        driver.get(arg1);
+	        browserObject = new ChromeDriver();
+	        browserObject.get(arg1);
 	        System.out.println(arg1 + "\t is the site");
 	}
 
@@ -71,7 +71,7 @@ WebDriver driver;
 	    // Write code here that turns the phrase above into concrete actions
 		 Thread.sleep(arg1 * 1000);
 	        System.out.println("wait of 5 secs");
-	        driver.close();
+	        browserObject.close();
 	}
 
 
