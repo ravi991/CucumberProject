68ce55e638a544391b8bd8322a79d058f8cb42dd
ravi991 <raviranjancs4@gmail.com>
2023-03-21 14:59:20 -0400

    Update MutipleScenario.java

--- MavenProject/src/test/java/stepDefinitions/MutipleScenario.java
+++ MavenProject/src/test/java/stepDefinitions/MutipleScenario.java
@@ -10,18 +10,18 @@ import cucumber.api.java.en.Then;
 public class MutipleScenario {
 WebDriver browserObject;
 	@Given("^user is on Google home page \"([^\"]*)\"$")
-	public void user_is_on_Google_home_page(String arg1) throws Throwable {
+	public void user_is_on_Google_home_page(String string) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
         browserObject = new ChromeDriver();
-        browserObject.get(arg1);
-        System.out.println(arg1 + "\t is the site");
+        browserObject.get(string);
+        System.out.println(string + "\t is the site");
 	}
 
 	@Then("^user enters keyword \"([^\"]*)\"$")
 	public void user_enters_keyword(String arg1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		browserObject.findElement(By.name("q")).sendKeys(arg1);
+		browserObject.findElement(By.name("q")).sendKeys(string);
         System.out.println(arg1 + "\t is the keyword");
 	}
 
@@ -33,43 +33,43 @@ WebDriver browserObject;
 	}
 
 	@Then("^user waits for (\\d+) seconds for google$")
-	public void user_waits_for_seconds_for_google(int arg1) throws Throwable {
+	public void user_waits_for_seconds_for_google(int int1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		Thread.sleep(arg1 * 1000);
+		Thread.sleep(int1 * 1000);
         System.out.println("wait of 5 secs");
         browserObject.close();
 	}
 
 	@Given("^user is on GitHub home page \"([^\"]*)\"$")
-	public void user_is_on_GitHub_home_page(String arg1) throws Throwable {
+	public void user_is_on_GitHub_home_page(String string) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Program Files\\\\chromedriver.exe");
         browserObject = new ChromeDriver();
-        browserObject.get(arg1);
-        System.out.println(arg1 + "\t is the site");
+        browserObject.get(string);
+        System.out.println(string + "\t is the site");
 	}
 
 	@Then("^user waits for (\\d+) seconds for github$")
-	public void user_waits_for_seconds_for_github(int arg1) throws Throwable {
+	public void user_waits_for_seconds_for_github(int int1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		Thread.sleep(arg1 * 1000);
+		Thread.sleep(int1 * 1000);
         System.out.println("wait of 5 secs");
         browserObject.close();
 	}
 
 	@Given("^user is on Twitter home page \"([^\"]*)\"$")
-	public void user_is_on_Twitter_home_page(String arg1) throws Throwable {
+	public void user_is_on_Twitter_home_page(String string) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		 System.setProperty("webdriver.chrome.driver", "C:\\\\Program Files\\\\chromedriver.exe");
 	        browserObject = new ChromeDriver();
-	        browserObject.get(arg1);
-	        System.out.println(arg1 + "\t is the site");
+	        browserObject.get(string);
+	        System.out.println(string + "\t is the site");
 	}
 
 	@Given("^user waits for (\\d+) seconds for twitter$")
-	public void user_waits_for_seconds_for_twitter(int arg1) throws Throwable {
+	public void user_waits_for_seconds_for_twitter(int int1) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		 Thread.sleep(arg1 * 1000);
+		 Thread.sleep(int1 * 1000);
 	        System.out.println("wait of 5 secs");
 	        browserObject.close();
 	}
