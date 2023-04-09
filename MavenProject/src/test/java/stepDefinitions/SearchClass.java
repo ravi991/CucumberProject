2bbb6db70bc19121d22f4dffb601f98f02524f38
ravi991 <raviranjancs4@gmail.com>
2023-04-09 09:24:01 -0400

    Update SearchClass.java

--- MavenProject/src/test/java/stepDefinitions/SearchClass.java
+++ MavenProject/src/test/java/stepDefinitions/SearchClass.java
@@ -14,7 +14,7 @@ public class SearchClass {
 	    // Write code here that turns the phrase above into concrete actions
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		ChromeOptions op = new ChromeOptions();
-op.addArguments("--remote-allow-origins=*");
+		op.addArguments("--remote-allow-origins=*");
 		browserObject = new ChromeDriver(op) ;
 		browserObject.get(string);
 	}
